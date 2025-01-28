33780ec2983c0b5042735efc9af31588fa5a8769
Jan Zenisek <janzenisek@gmx.at>
2025-01-28 19:27:18 +0100

    added heartbeat entity

--- src/Entities/Application.cs
+++ src/Entities/Application.cs
@@ -4,8 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Ai.Hgb.Common.Entities {
+
+  // TODO: https://ms.codes/blogs/computer-hardware/c-get-cpu-usage-of-process#:~:text=Process%20%2D%20Key%20Takeaways-,To%20get%20the%20CPU%20usage%20of%20a%20process%20in%20C%23,processor%20time%20for%20the%20process.
+  public class Heartbeat {
+    public string Id { get; set; }
+    public DateTime Timestamp { get; set; }
+    public string ApplicationId { get; set; }
+    public string ApplicationName { get; set;}
+    public int CpuUtilization { get; set; }
+    public int MemoryUtilization { get; set; }
+    public int GpuUtilization { get; set; }
+
+    public Heartbeat() { }
+
+    public override string ToString() {
+      return $"{Timestamp}:\t{CpuUtilization}\t{MemoryUtilization}\t{GpuUtilization}";
+    }
+  }
+
   public class ApplicationParametersBase {
     [JsonPropertyName("name")]
     public string Name { get; set; }
